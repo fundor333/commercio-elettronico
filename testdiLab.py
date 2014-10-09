50170de4b04443178c75f10fcd1c74e120ef936f
Fundor333 <fundor333@gmail.com>
2014-10-09 14:24:35 +0200

    Aggiornamento per la prima esercitazione

--- testdiLab.py
+++ testdiLab.py
@@ -1,76 +1,35 @@
 #!/usr/bin/python
-import urllib2
+import urllib
+# import lxml.html as H
+# import lxml.etree as ET
+import BeautifulSoup
+# import codecs
+
+class WebPage:
+    filen = ""
+
+    def __init__(self, url, name):
+        urllib.urlretrieve(url, name)
+        self.filen = name
+
+    def decode_html(filename):
+        html_string = file(filename).read()
+        converted = BeautifulSoup.UnicodeDammit(html_string, isHTML=True)
+        if not converted.unicode:
+            return ''
+        # print converted.originalEncoding
+        return converted.unicode
 
 
-class LettoreFile:
-    input = ""
-    output = ""
-
-    def __init__(self, i, o):
-        self.input = urllib2.urlopen(i)
-        self.output = open(o, 'w')
-
-    def elaboratore(self):
-        for line in self.input:
-            self.output = self.output + self.pulisciparola(line)
-        return self
-
-    def pulisciparola(self,word):
-        #word= word.split('\W')
-        return word
-
-    def tuttomaiuscolo(self):
-        for line in self.input:
-            self.output.write(line.upper())
-
-    def chiudi(self):
-        self.input.close()
-        self.output.close()
-
-
-class GestoreCollezioneFile:
-    listafiletot = None
-    dizionario = {}
-
-    def __init__(self, listafile):
-        self.listafiletot = listafile
-        for singlefile in listafile:
-            self.aggiorna(open(singlefile, 'r'))
-
-    def aggiorna(self, nomefile):
-        lines = nomefile.readlines()
-        mappa = {}
-
-        for line in lines:
-            splitted = line.upper()
-            splitted = splitted.split()
-            for word in splitted:
-                if word in mappa:
-                    mappa[word] = (mappa[word][0] + 1, mappa[word][1] + 1)
-                else:
-                    mappa[word] = (1, 1)
-
-        for keyword in mappa.keys():
-            if keyword in self.dizionario:
-                self.dizionario[keyword] = (self.dizionario[keyword][0] + 1, self.dizionario[keyword][1] + 1)
-            else:
-                self.dizionario[keyword] = (1, 1)
-
-    def printer(self):
-        f = open('dizout.txt', 'w')
-        for x in self.dizionario:
-            f.write(str(self.dizionario[x][0])+" "+str(self.dizionario[x][1])+' '+x+'\n')
-        f.close()
+class AppURLopener(urllib.FancyURLopener):
+    version = "App/1.7"
 
 
 def main():
-    lista = ('output1.txt', 'output2.txt', 'output3.txt')
-
-    LettoreFile('http://www.unive.it/data/46/1', lista[0])
-    LettoreFile('http://www.unive.it/data/46/2', lista[1])
-    LettoreFile('http://www.unive.it/data/46/3', lista[2])
-
-    GestoreCollezioneFile(lista).printer()
+    urllib._urlopener = AppURLopener()
+    web = WebPage(
+        "https://www.google.it/search?q=site%3Awww.ansa.it+mafia&btnG=Cerca&gbv=1",
+        "risultati.html")
 
 # Esecutore intero progetto
 if __name__ == "__main__":
