c0e5a370db2578e557678b84062cd3e7131bcc31
Matteo Scarpa <matteoscarpa92@gmail.com>
2014-10-23 23:04:41 +0200

    Calcola i coseni di tutte le combinazioni di file

--- Seconda Consegna/__init__.py
+++ Seconda Consegna/__init__.py
@@ -8,7 +8,7 @@ NUM = 665
 ELENCOFILE = range(NUM)
 
 
-class confrontaarray:
+class Confrontaarray:
     dizionario1 = ""
     dizionario2 = ""
     x = 0
@@ -18,19 +18,20 @@ class confrontaarray:
     def __init__(self, nomefile1, nomefile2):
         self.dizionario1 = DIZIONARIOTOTALE[nomefile1]
         self.dizionario2 = DIZIONARIOTOTALE[nomefile2]
-        self.calcolacoseno()
 
     def calcolacoseno(self):
         for singolakey in self.dizionario1.keys():
-            self.x = self.x + self.dizionario1[singolakey]* self.dizionario1[singolakey]
+            self.x = self.x + self.dizionario1[singolakey] * self.dizionario1[singolakey]
             if singolakey in self.dizionario2.keys():
-                self.xy = self.xy + self.dizionario1[singolakey]*self.dizionario2[singolakey]
+                self.xy = self.xy + self.dizionario1[singolakey] * self.dizionario2[singolakey]
         for singolakey in self.dizionario2.keys():
-            self.y = self.y + self.dizionario2[singolakey]* self.dizionario2[singolakey]
+            self.y = self.y + self.dizionario2[singolakey] * self.dizionario2[singolakey]
         self.x = sqrt(self.x)
         self.y = sqrt(self.y)
-        coseno = (self.xy)/(self.x * self.y)
-        print(coseno)
+        try:
+            coseno = self.xy / (self.x * self.y)
+        except ZeroDivisionError:
+            coseno = 0
         return coseno
 
 
@@ -39,21 +40,21 @@ class Lettorefile:
 
     def __init__(self, listafile):
         self.listanomefile = listafile
-        for singolofile in listafile:
-            self.readerpage(singolofile)
+        self.readerpage()
 
-    def readerpage(self, nomefile):
-        singlefile = open(nomefile, 'r')
-        dictionary = {}
-        for line in singlefile:
-            for parolanonelaborata in line.split():
-                for singolaparola in re.split("[^a-zA-Z]", parolanonelaborata):
-                    if singolaparola != "":
-                        if singolaparola in dictionary.keys():
-                            dictionary[singolaparola.lower()] += 1
-                        else:
-                            dictionary[singolaparola.lower()] = 1
-        DIZIONARIOTOTALE[nomefile] = dictionary
+    def readerpage(self):
+        for nomefile in self.listanomefile:
+            singlefile = open(nomefile, 'r')
+            dictionary = {}
+            for line in singlefile:
+                for parolanonelaborata in line.split():
+                    for singolaparola in re.split("[^a-zA-Z]", parolanonelaborata):
+                        if singolaparola != "":
+                            if singolaparola in dictionary.keys():
+                                dictionary[singolaparola.lower()] += 1
+                            else:
+                                dictionary[singolaparola.lower()] = 1
+            DIZIONARIOTOTALE[nomefile] = dictionary
 
 
 def main():
@@ -61,7 +62,16 @@ def main():
         ELENCOFILE[i] = "risultati_" + str(i) + "_changed.txt"
 
     Lettorefile(ELENCOFILE)
-    confrontaarray("risultati_5_changed.txt","risultati_10_changed.txt")
+    file_out = open("output.txt", 'w')
+
+    for i in range(0, len(ELENCOFILE)):
+        print("Giro numero " + str(i) + " su " + str(NUM))
+        for y in range(i + 1, len(ELENCOFILE)):
+            numero = Confrontaarray("risultati_" + str(i) + "_changed.txt",
+                                    "risultati_" + str(y) + "_changed.txt").calcolacoseno()
+            file_out.write(str(numero) + "\n")
+
+    file_out.close()
 
 # Esecutore intero progetto
 if __name__ == "__main__":
