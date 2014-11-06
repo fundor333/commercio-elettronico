7041a566c0e78545a6a33269ad77874cdb3b51f0
Matteo Scarpa <matteoscarpa92@gmail.com>
2014-11-06 12:43:17 +0100

    Piccola correzione Prof
    Ho sbagliato funzione da usare per il calcolo della distanza. Ora
    corretto

--- Seconda Consegna Bis/__init__.py
+++ Seconda Consegna Bis/__init__.py
@@ -3,7 +3,7 @@ from numpy.ma import concatenate, vstack
 __author__ = 'Fundor333'
 
 import numpy
-from scipy.spatial.distance import pdist
+from scipy.spatial.distance import cosine
 
 def arrayrandom(number):
     return numpy.random.random(number)
@@ -16,12 +16,11 @@ def matrixrandom(xsize, ysize):
 def multiplayer(array1, array2):
     return numpy.dot(array1, array2)
 
-def distancecos(array1, array2):
-    z= numpy.vstack([array1,array2])
-    return pdist(z,'cosine')
+def distancecof(array1, array2):
+    return cosine(array1,array2)
 
 
 if __name__ == "__main__":
     print(arrayrandom(10))
     print()
-    print(distancecos(arrayrandom(10), arrayrandom(10)))
+    print(distancecof(arrayrandom(10), arrayrandom(10)))
