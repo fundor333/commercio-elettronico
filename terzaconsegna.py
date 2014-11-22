461c094dbe4b7adc2614f5aaf3d16650af00f163
Fundor333 <fundor333@gmail.com>
2014-11-22 14:48:22 +0100

    Cambiamento del codice contenuto nel gitignore

--- terzaconsegna.py
+++ terzaconsegna.py
@@ -1,9 +1,31 @@
 __author__ = 'Fundor333'
 
-from DatabaseMongoClass import databasemongo
+import linecache
+
+from DatabaseMongoClass import database
+from primaconsegna import getfromgoogle, NUMERORISULTATI, OUTPITFILENAME
+
+NAMEDB = "Silvestri"
+DBM = database(NAMEDB, 'localhost', 27017)
+
+
+def elaboratejson(texts, namefile):
+    return {"filename": namefile, "text": texts}
+
 
 def main():
-    dbM = databasemongo('localhost', 27017)
+    try:
+        fistline = linecache.getline("./out/" + OUTPITFILENAME + ".txt", 1)
+    except IOError:
+        fistline = getfromgoogle(NUMERORISULTATI)
+
+    for i in range(0, int(fistline)):
+        textappend = ""
+        fileinname = "./out/" + str(i) + ".txt"
+        filein = open(fileinname)
+        for line in filein:
+            textappend += line
+        print(DBM.insertdocument(elaboratejson(textappend, fileinname)))
 
 
 if __name__ == "__main__":
