c2d479aca477f8313fb8b9f02687bf608d73e40c
Matteo Scarpa <matteoscarpa92@gmail.com>
2014-12-04 17:17:40 +0100

    Aggiunto codice per Twitter

---
+++ twitter.py
@@ -0,0 +1,32 @@
+__author__ = 'Fundor333'
+
+from twython import TwythonStreamer
+
+from DatabaseMongoClass import database
+from datitwitter import APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET
+
+
+NAMEDB = "twitter"
+DBM = database(NAMEDB, 'localhost', 27017)
+
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print data['text'].encode('utf-8')
+
+    def on_error(self, status_code, data):
+        print status_code
+
+        # Want to stop trying to get data because of the error?
+        # Uncomment the next line!
+        self.disconnect()
+
+
+def main():
+    stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    stream.statuses.filter(locations="10.623150,44.791538,13.101060,46.680580")
+
+
+if __name__ == '__main__':
+    main()
