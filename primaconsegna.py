926906f3514fce55b557ace2c18ea085615636e7
Fundor333 <fundor333@gmail.com>
2015-06-02 11:09:55 +0200

    Rebuilding the fist exercitation

--- primaconsegna.py
+++ primaconsegna.py
@@ -1,25 +1,20 @@
-import urllib
-from urllib.request import FancyURLopener
-from bs4 import BeautifulSoup
 from google import search
 import requests
+import lxml.html
 
 SESSION = requests.Session()
 GOOGLE_SEARCH_STRING = "site:www.repubblica.it + politica OR crisi OR calcio OR articolo OR article"
-NUMERORISULTATI = 1
+NUMERORISULTATI = 120
 WAITINGTIME = 2  # in secondi
-QUERYGOOGLE = '//h3[@class="r"]/a/@href'
 QUERYSITO = '//*[@itemprop="articleBody"]/text()'
 
 
 # ####################################################
 
 article_url=search(GOOGLE_SEARCH_STRING, stop=NUMERORISULTATI, pause=WAITINGTIME)
-print(article_url)
+text=[]
 for url in article_url:
-    html_page = urllib.request.urlopen(url)
-    html_string = html_page.read()
-    converted = BeautifulSoup.UnicodeDammit(html_string, isHTML=True)
-    if not converted.str:
-        print("")
-    print(converted.str)
+    html = lxml.html.parse(url)
+    packages = html.xpath(QUERYSITO)
+    if packages:
+        text.append(packages)
